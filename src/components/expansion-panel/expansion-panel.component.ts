b26df66d23d7778a42413c59de57bc85e76e04d4
randyangeles <rangeles@camspro.com>
2017-04-17 11:04:07 -0700

    Change to use BrowserAnimationsModule.

--- src/components/expansion-panel/expansion-panel.component.ts
+++ src/components/expansion-panel/expansion-panel.component.ts
@@ -17,7 +17,7 @@ import {
   HostListener
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'mdl-expansion-panel-header',
@@ -68,9 +68,15 @@ export class MdlExpansionPanelHeaderSecondaryContentComponent { }
   },
   animations: [
     trigger('isExpanded', [
-      state('true', style({ height: '*' })),
-      state('false', style({ height: '0px' })),
-      transition('* => *', animate('250ms ease-in'))
+      state('true', style({ height: '85px', overflow: 'auto' })),
+      state('false', style({ height: '0'})),
+	    transition('true => false', [
+		    style({ overflow: 'hidden' }),
+	  	  animate('300ms ease-in', style({ height: '0' })),
+	    ]),
+	    transition('false => true', [
+	  	  animate('300ms ease-out', style({ height: '85px' })),
+	    ])
     ])
   ]
 })
@@ -205,7 +211,7 @@ const MDL_EXPANSION_PANEL_DIRECTIVES = [
 ];
 
 @NgModule({
-  imports: [CommonModule, NoopAnimationsModule ],
+  imports: [CommonModule, BrowserAnimationsModule ],
   exports: MDL_EXPANSION_PANEL_DIRECTIVES,
   declarations: MDL_EXPANSION_PANEL_DIRECTIVES,
 })
